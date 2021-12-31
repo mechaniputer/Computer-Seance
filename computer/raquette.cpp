bdc4d54a920c68d5c54f3fd0698bfd5aab8c555e
hbeadle <hbeadle@cs.rochester.edu>
2021-12-31 09:02:24 -0500

    More keys, undefined instruction increments PC

--- computer/raquette.cpp
+++ computer/raquette.cpp
@@ -1418,6 +1418,7 @@ int Raquette::step(bool verbose) {
 
 		default:
 			std::cout << "Undefined instruction:" << std::hex << (int) thisbyte << std::dec << std::endl;
+			pc++;
 			return 1;
 			break;
 	}
